587f439dfab37174a9cdcc50765398c7263b5bd6
asifkamal <asifkamal530@gmail.com>
2024-09-11 23:39:16 +0600

    term query with multiple data

--- src/main/java/com/elasticsearch/elastic/ElasticQuery/ElasticsearchOperations/OperationController.java
+++ src/main/java/com/elasticsearch/elastic/ElasticQuery/ElasticsearchOperations/OperationController.java
@@ -1,8 +1,10 @@
 package com.elasticsearch.elastic.ElasticQuery.ElasticsearchOperations;
 
+import co.elastic.clients.elasticsearch._types.FieldValue;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.util.ObjectBuilder;
 import com.elasticsearch.elastic.Result.SearchHits;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,8 +18,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @RestController
@@ -44,7 +51,26 @@ public class OperationController {
 
        List<?> searchHits =  elasticsearchOperations.search(nativeQuery, SearchHits.class, IndexCoordinates.of(elkIndex)).getSearchHits().stream().map(SearchHit::getContent).toList();
 
+    }
 
+    @GetMapping("searchTerm")
+    private void termQuery() throws IOException{
+        NativeQueryBuilder nativeQueryBuilder = new NativeQueryBuilder();
+        List <String> bedGroups = new ArrayList<>(List.of(new String[]{"King Bed", "Queen Bed"}));
+
+        List<FieldValue> bed_groups = new ArrayList<>();
+        bedGroups.forEach(bd -> bed_groups.add(FieldValue.of(bd)));
+
+        nativeQueryBuilder.withQuery(
+                q-> q.bool(
+                bq->bq.should(
+                sq->sq.terms(
+                t-> t.field("bed_type").terms(term -> term.value(bed_groups))
+        ))));
+        NativeQuery nativeQuery = nativeQueryBuilder.build();
+
+        List<?> searchHits =  elasticsearchOperations.search(nativeQuery, SearchHits.class, IndexCoordinates.of("hotel")).getSearchHits().stream().map(SearchHit::getContent).toList();
+        searchHits.stream().filter(x-> Boolean.parseBoolean(x.toString()));
     }
 
 }
