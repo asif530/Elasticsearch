5122f3f03a83e4c16667dd291d8ea19c20a2b34d
asifkamal <asifkamal530@gmail.com>
2024-12-11 07:30:05 +0600

    trying to aggregate the results

--- src/main/java/com/elasticsearch/elastic/ElasticQuery/ElasticsearchOperations/OperationController.java
+++ src/main/java/com/elasticsearch/elastic/ElasticQuery/ElasticsearchOperations/OperationController.java
@@ -1,11 +1,21 @@
 package com.elasticsearch.elastic.ElasticQuery.ElasticsearchOperations;
 
 import co.elastic.clients.elasticsearch._types.FieldValue;
+import co.elastic.clients.elasticsearch._types.aggregations.Aggregate;
 import co.elastic.clients.elasticsearch._types.aggregations.Aggregation;
+import co.elastic.clients.elasticsearch._types.aggregations.DoubleTermsAggregate;
+import co.elastic.clients.elasticsearch._types.aggregations.FilterAggregate;
 import com.elasticsearch.elastic.Result.SearchHits;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchAggregation;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchAggregations;
 import org.springframework.data.elasticsearch.client.elc.NativeQuery;
 import org.springframework.data.elasticsearch.client.elc.NativeQueryBuilder;
+import org.springframework.data.elasticsearch.core.AggregationsContainer;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
@@ -14,9 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("elkOperations")
@@ -27,6 +35,9 @@ public class OperationController {
 
     @Value("${elk-index}")
     private String elkIndex;
+
+    //private String elasticIndex = "hotel";
+    private String elasticIndex = "index_demo_phx_property_v2";
 
     public OperationController(ElasticsearchOperations elasticsearchOperations,SearchHits result) {
         this.elasticsearchOperations = elasticsearchOperations;
@@ -52,27 +63,49 @@ public class OperationController {
         List<FieldValue> bed_groups = new ArrayList<>();
         bedGroups.forEach(bd -> bed_groups.add(FieldValue.of(bd)));
 
-        nativeQueryBuilder.withQuery(
+      /*  nativeQueryBuilder.withQuery(
                 q-> q.bool(
                 bq->bq.should(
                 sq->sq.terms(
                 t-> t.field("bed_type").terms(term -> term.value(bed_groups))
-        ))));
-        NativeQuery nativeQuery = nativeQueryBuilder.build();
-       //Aggregation on fields
-        aggregationOnArrayType();
+        ))));*/
+        //Aggregation on fields
+        aggregationOnArrayType(nativeQueryBuilder);
 
-        List<?> searchHits =  elasticsearchOperations.search(nativeQuery, SearchHits.class, IndexCoordinates.of("hotel")).getSearchHits().stream().map(SearchHit::getContent).toList();
+        NativeQuery nativeQuery = nativeQueryBuilder.build();
+
+        AggregationsContainer elastic = elasticsearchOperations.search(nativeQuery, SearchHits.class, IndexCoordinates.of(elasticIndex)).getAggregations();
+        transformResponse(elastic);
+        List<?> searchHits =  elasticsearchOperations.search(nativeQuery, SearchHits.class, IndexCoordinates.of(elasticIndex)).getSearchHits().stream().map(SearchHit::getContent).toList();
         searchHits.stream().filter(x-> Boolean.parseBoolean(x.toString()));
     }
 
-    private void aggregationOnArrayType(){
-        NativeQueryBuilder nativeQueryBuilder = new NativeQueryBuilder();
-        nativeQueryBuilder.withAggregation("bed_type_count", Aggregation.of(agg-> agg.terms(t->t.field("bed_type"))));
-        NativeQuery nativeQuery = nativeQueryBuilder.build();
+    private void aggregationOnArrayType(NativeQueryBuilder nativeQueryBuilder){
+        //NativeQueryBuilder nativeQueryBuilder = new NativeQueryBuilder();
+        nativeQueryBuilder.withAggregation("filterS", new Aggregation.Builder().filter(a->a.term(m->m.field("country_code").value("BD"))).aggregations("star_rating", Aggregation.of(agg-> agg.terms(t->t.field("star_rating")))).build());
+        //nativeQueryBuilder.withAggregation("bed_type_count", Aggregation.of(agg-> agg.terms(t->t.field("bed_type"))));
+        //nativeQueryBuilder.withAggregation("star_rating", Aggregation.of(agg-> agg.terms(t->t.field("star_rating"))));
+        //NativeQuery nativeQuery = nativeQueryBuilder.build();
 
-        List<?> searchHits = Collections.singletonList(elasticsearchOperations.search(nativeQuery, SearchHits.class, IndexCoordinates.of("hotel")));
-        searchHits.stream().forEach(Object::toString);
+       /* List<?> searchHits = Collections.singletonList(elasticsearchOperations.search(nativeQuery, SearchHits.class, IndexCoordinates.of("hotel")));
+        searchHits.stream().forEach(Object::toString);*/
+    }
+
+    public JsonNode transformResponse(AggregationsContainer container) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode transformedResponse = mapper.createObjectNode();
+        Map<String, ElasticsearchAggregation> mapAggregation = ((ElasticsearchAggregations) container).aggregationsAsMap();
+        List<ElasticsearchAggregation> aggregationList = ((ElasticsearchAggregations) container).aggregations();
+        Object result = aggregationList.get(0).aggregation().getAggregate()._get();
+
+        if(result instanceof FilterAggregate){
+
+            Aggregate dterm = ((FilterAggregate) result).aggregations().get("star_rating");
+            JsonNode originalJsonNode = mapper.readTree(dterm.dterms().buckets().toString());
+        }
+
+
+        return null;
     }
 
 }
